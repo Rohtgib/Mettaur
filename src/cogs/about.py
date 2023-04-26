7a910eb22def28b269a8f766f1b313815f7f7bcc
Victor Gomez <vicgmezp@gmail.com>
2023-04-26 11:18:11 -0400

    Cleaned about.py
    Removed about command

--- src/cogs/about.py
+++ src/cogs/about.py
@@ -14,10 +14,6 @@ class About(commands.Cog):
     async def on_ready(self):
         print(f"{filename} loaded")
 
-    @commands.command()
-    async def about(self, ctx):
-        await ctx.send("I am Mettaur")
-
 
 async def setup(bot):
     await bot.add_cog(About(bot))
