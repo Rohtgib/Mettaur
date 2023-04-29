551034134f581776178b97398739666184d0bbf1
GitHub Action <action@github.com>
2023-04-29 21:44:10 +0000

    Formatted with Black

--- src/cogs/about.py
+++ src/cogs/about.py
@@ -21,19 +21,33 @@ class aboutCog(commands.Cog):
     @commands.command()
     async def server(self, ctx):
         creationDate = datetimeDiscord(ctx.guild.created_at)
-        serverRoles = [role.mention for role in ctx.guild.roles if role.name != "@everyone"]
-        embed=discord.Embed()
+        serverRoles = [
+            role.mention for role in ctx.guild.roles if role.name != "@everyone"
+        ]
+        embed = discord.Embed()
         embed.set_author(name=f"{ctx.guild.name}", icon_url=f"{ctx.guild.icon.url}")
         embed.set_thumbnail(url=ctx.guild.icon.url)
         embed.add_field(name="Owner", value=f"{ctx.guild.owner}", inline=False)
-        embed.add_field(name="Member Count", value=f"{ctx.guild.member_count}", inline=True)
-        embed.add_field(name="Text Channels", value=f"{len(ctx.guild.text_channels)}", inline=True)
-        embed.add_field(name="Voice Channels", value=f"{len(ctx.guild.voice_channels)}", inline=True)
+        embed.add_field(
+            name="Member Count", value=f"{ctx.guild.member_count}", inline=True
+        )
+        embed.add_field(
+            name="Text Channels", value=f"{len(ctx.guild.text_channels)}", inline=True
+        )
+        embed.add_field(
+            name="Voice Channels", value=f"{len(ctx.guild.voice_channels)}", inline=True
+        )
         embed.add_field(name="Emotes", value=f"{len(ctx.guild.emojis)}", inline=True)
-        embed.add_field(name="Stickers", value=f"{len(ctx.guild.stickers)}", inline=True)
+        embed.add_field(
+            name="Stickers", value=f"{len(ctx.guild.stickers)}", inline=True
+        )
         embed.add_field(name="Roles", value=f"{len(ctx.guild.roles)-1}", inline=True)
-        embed.add_field(name="Role List", value=f"{' '.join(serverRoles)}", inline=False)
-        embed.set_footer(text=f"Server ID: {ctx.guild.id} | Server created at {creationDate.formatted_date}")
+        embed.add_field(
+            name="Role List", value=f"{' '.join(serverRoles)}", inline=False
+        )
+        embed.set_footer(
+            text=f"Server ID: {ctx.guild.id} | Server created at {creationDate.formatted_date}"
+        )
         await ctx.send(embed=embed)
 
     @server.error
