686e66c656612999721bffe66386d2b18dc307de
Victor Gomez <vicgmezp@gmail.com>
2023-05-11 20:36:42 -0400

    Grab random user from the server list

--- src/cogs/miscellaneous.py
+++ src/cogs/miscellaneous.py
@@ -1,8 +1,10 @@
 import os
 import discord
+import random
 from errors import injected
 from discord.ext import commands
 from config import Config
+# from moderation import ban, ban_error, kick, kick_error
 
 path = __file__
 filename = os.path.basename(path)
@@ -15,6 +17,29 @@ class miscellaneousCog(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print(f"{filename[:-3].capitalize()} cog loaded")
+
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def randomban(self, ctx):
+        server = ctx.guild
+        user = random.choice(server.members)
+        await ctx.send(f"{user} I see you.")
+#        await ban(self, ctx, user,"Bad RNG")
+    
+    @randomban.error
+    async def randomban_error(self, ctx, error):
+        await injected(ctx,error)
+
+    @commands.command()
+    @commands.has_permissions(kick_members=True)
+    async def randomkick(self, ctx):
+        server = ctx.guild
+        user = random.choice(server.members)
+        await ctx.send(f"{user} I see you.")
+    
+    @randomkick.error
+    async def randomkick_error(self, ctx, error):
+        await injected(ctx,error)
 
 
 async def setup(bot):
